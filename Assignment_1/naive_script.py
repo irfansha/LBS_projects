bb1369167f21bd0af76798dc735465d2d7cce633
Irfansha <irfansha536@gmail.com>
2020-02-03 14:56:51 +0100

    Started outline of native script with todos.

---
+++ Assignment_1/naive_script.py
@@ -0,0 +1,27 @@
+# Irfansha Shaik, 03.02.2020, Aarhus
+
+'''
+Emilie Lykke Thonsgaard
+Irfansha Shaik
+'''
+
+# A naive script for submitting username and accessing the response from the url:
+# http://lbs-2020-02.askarov.net:3030/
+
+'''
+todos:
+- Write a function for submitting username: test_username
+  Input: string (username)
+  Proc: we submit the username in the website and time the invocation
+  Ouput: float (the time taken to complete invocation)
+'''
+
+def test():
+  print("Yup!")
+
+
+def main():
+  test()
+
+if __name__== "__main__":
+  main()
