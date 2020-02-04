78cc5989fe65555027b65afb4fc4492511a3f25c
Irfansha <irfansha536@gmail.com>
2020-02-04 15:57:28 +0100

    Function added to create a list of usernames. - started updating the testing function to include timing

--- Assignment_1/naive_script.py
+++ Assignment_1/naive_script.py
@@ -14,19 +14,31 @@ todos:
   Input: string (username)
   Proc: we submit the username in the website and time the invocation
   Ouput: float (the time taken to complete invocation)
+- Now need to time the calls
+  - We want to use inbuild time functions in python.
+  - First we consider the module timeit.
 '''
 
 import requests
 
+def get_usernames():
+  users = []
+  users.append("admin")
+  return users
 
-def test_username():
-  payload= {'username': 'admin', 'Submit':'submit'}
+def test_username(username):
+  payload= {'username': username, 'Submit':'submit'}
   #r = requests.get('http://lbs-2020-02.askarov.net:3030/', data=data)
   r1 = requests.post('http://lbs-2020-02.askarov.net:3030/reset/', payload)
-  print(r1.text)
+  # We need to assert if the resultant string is a success
+  #print(r1.text)
+  return 0
 
 def main():
-  test_username()
+  usernames = get_usernames()
+  for user_name in usernames:
+    user_time = test_username(user_name)
+    #print(user_time)
 
 if __name__== "__main__":
   main()
